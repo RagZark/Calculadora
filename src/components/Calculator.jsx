a283af8edb54a3c23eaf995e9dfebae0c702dd1d
Danver <danver.ver@hotmail.com>
2024-10-12 16:18:51 -0300

    First functions, to set a number and clear

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import './Calculator.css';
 import { Box, Container } from "@mui/material";
 
 const Calculator = () => {
+    const [num, setNum] = useState(0)
     const buttonValues = ["AC", "+/-", "%", "/", 7, 8, 9, "X", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", "ç", "="];
 
     const colorKeys = (key) => {
         switch (true) {
-            case (key >= 0 && key <= 9 ) || key === ".":
+            case (key >= 0 && key <= 9) || key === ".":
                 return "gray";
             case key === "AC" || key === "+/-" || key === "%":
                 return "white";
@@ -17,25 +18,41 @@ const Calculator = () => {
                 return "orange";
         }
     };
-    
 
+    const inputNum = (e) => {
+        let input = e.target.value
+        setNum((num === 0 || input === ".") && (input >= 0 && input <= 9) ? input : num + input)
+    }
+
+    const clearInput = (e) => {
+        setNum(0)
+    }
+
+
+    const handleClick = (e) => {
+        e.target.value === "AC" ? clearInput(e) : inputNum(e)
+    }
+    
     return (
         <div>
-        <Box m={5}/>
-        <Container maxWidth="xs" >
-            <div className="wrapper">
-            <h1 style={{color: "#000", display: "flex", justifyContent: "flex-end", paddingRight: "0.1em", fontSize: "5em", backgroundColor: "#FFF", border: "2px solid #ccc"}}>01</h1>
-                {buttonValues.map((element, index) => (
-                    <button 
-                        key={index} 
-                        className={colorKeys(element)}
-                        value={element}
-                    >
-                        {element}
-                    </button>
-                ))}
-            </div>
-        </Container>
+            <Box m={5} />
+            <Container maxWidth="xs" >
+                <div className="wrapper">
+                    <h1 style={{ color: "#000", display: "flex", justifyContent: "flex-end", paddingRight: "0.1em", fontSize: "5em", backgroundColor: "#FFF", border: "2px solid #ccc" }}>
+                        {num}
+                    </h1>
+                    {buttonValues.map((element, index) => (
+                        <button
+                            key={index}
+                            className={colorKeys(element)}
+                            value={element}
+                            onClick={handleClick}
+                        >
+                            {element}
+                        </button>
+                    ))}
+                </div>
+            </Container>
         </div>
     );
 };
